e4ccb3c2c82bc3e0d8e763ea7c42b217257253ba
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-16 19:58:20 +0530

    global error middleware is added

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,4 +19,17 @@ app.use(cors({
 
 app.use('/api/auth',authRoutes)
 
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+
+  const message = err.message || "Internal Server Error"
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
+
 export default app;
