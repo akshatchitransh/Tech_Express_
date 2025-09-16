8f0da8abe63b13a45528393f9cb6228ca76cbadf
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-16 22:47:10 +0530

    sign-in backend functionality is done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcryptjs from "bcryptjs"
 import User from "../models/user.model.js"
 import { asyncHandler } from "../utils/async-handler.js"
 import { ApiError } from "../utils/api-error.js"
+import jwt from "jsonwebtoken"
 
 export const signup = asyncHandler(async (req, res,next) => {
   const { username, email, password } = req.body
@@ -33,8 +34,37 @@ export const signup = asyncHandler(async (req, res,next) => {
 
 })
 
-export const signin = ()=>{
+export const signin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
 
-}
+  if (!email || !password || email === "" || password === "") {
+   throw new ApiError(400 , "All fields are required")
+  }
+
+  try {
+    const validUser = await User.findOne({ email })
+
+    if (!validUser) {
+     throw new ApiError(400 , "User not found")
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+    if (!validPassword) {
+     throw new ApiError(400 , "Wrong credentials")
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    )
+
+   // const { password: pass, ...rest } = validUser._doc
+
+    res.status(200).cookie("access_token", token, { httpOnly: true , secure:true }).json({msg:"loggedin"})
+  } catch (error) {
+    next(error)
+  }
+})
 
 
