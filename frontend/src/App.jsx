c44f3615aa97a3f65b61ced104fbd7d5daa9ea5f
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-16 18:20:35 +0530

    Pages Structure and routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Button } from "@/components/ui/button"
+
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import SignInForm from './auth/forms/SignInForm'
+import SignUpForm from './auth/forms/SignUpForm'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import NewsArticle from './pages/NewsArticle'
+import Header from './components/shared/Header'
+import Footer from './components/shared/Footer'
 const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button className={"bg-amber-700"}>Click me</Button>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/news' element={<NewsArticle />} />
+      </Routes>
+<Footer/>
+      
+    </BrowserRouter>
+
+    
   )
 }
 
---
+++ frontend/src/pages/NewsArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsArticle = () => {
+  return (
+    <div>
+      News
+    </div>
+  )
+}
+
+export default NewsArticle
