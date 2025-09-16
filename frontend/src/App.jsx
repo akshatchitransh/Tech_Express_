b9a390beb0c518a058b8ed11be4cdec1d3855864
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-16 18:07:35 +0530

    Shad cn UI installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className={"bg-amber-700"}>Click me</Button>
+    </div>
+  )
+}
+
+export default App
