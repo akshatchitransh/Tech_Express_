0af3c7911438430f140bf7fa824a65a3525be2ea
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-19 21:15:59 +0530

    private route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import About from './pages/About'
 import NewsArticle from './pages/NewsArticle'
 import Header from './components/shared/Header'
 import Footer from './components/shared/Footer'
+import PrivateRoute from './components/shared/PrivateRoute'
 const App = () => {
   return (
     <BrowserRouter>
@@ -17,7 +18,9 @@ const App = () => {
         <Route path='/sign-in' element={<SignInForm />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/' element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+          <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path='/about' element={<About />} />
         <Route path='/news' element={<NewsArticle />} />
       </Routes>
