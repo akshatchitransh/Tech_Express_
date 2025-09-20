fae5b4bc8a3704eb472fd1b99807daf04f7a422a
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-09-20 22:07:50 +0530

    partial image upload system created using appwrite and may be changed to cloudinary

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import BottomNavBar from "@/components/shared/BottomNavBar"
+//import DashboardComments from "@/components/shared/DashboardComments"
+//import DashboardPosts from "@/components/shared/DashboardPosts "
+import DashboardProfile from "@/components/shared/DashboardProfile"
+import DashboardSidebar from "@/components/shared/DashboardSidebar"
+//import DashboardUsers from "@/components/shared/DashboardUsers"
+//import MainDashboard from "@/components/shared/MainDashboard"
+import React, { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const Dashboard = () => {
+  const location = useLocation()
+  const [tab, setTab] = useState("")
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get("tab")
+
+    
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
+
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col md:flex-row w-full">
+      
+      <div className="hidden md:block">
+        <DashboardSidebar />
+      </div>
+
+      <BottomNavBar />
+
+      {<div className="w-full">
+        
+        {tab === "profile" && <DashboardProfile />}
+
+        
+        {/* {tab === "posts" && <DashboardPosts />}
+
+       
+        {tab === "users" && <DashboardUsers />}
+
+        
+        {tab === "comments" && <DashboardComments />}
+
+        
+        {tab === "dashboard" && <MainDashboard />} */}
+      </div> }
     </div>
   )
 }
